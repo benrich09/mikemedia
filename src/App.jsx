5e6ab2cea1d8d94e2113ee252bb60fb082c3b00a
BEN RICH <benrich205@gmail.com>
2025-07-29 13:20:22 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import Navbar from 'Navabar.jsx';
-import Header from 'Header';
+import {useState} from "react";
+import {Navbar} from "./Components/Navbar.jsx";
+import {MobileMenu} from "./Components/MobileMenu.jsx";
+import {LoadingScreen} from "./Components/LoadingScreen.jsx";
+import {Home} from "./Components/section/Home.jsx"
 import './App.css'
 
 function App() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
-  return (
+return (
     <>
+        {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} {" "}
+
+        <div className={`min-h-screen transition-opacity duration-700
+       ${isLoaded ? "opacity-100" : "opacity-0"} bg-whitesmoke text-black-100`}>
+
+            <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+
+            <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+
+            <Home/>
+
+        </div>
+
 
 
     </>
---
+++ src/Components/section/Home.jsx
@@ -0,0 +1,42 @@
+import {RevealOnScroll} from "../RevealOnScroll.jsx";
+
+export const Home = () => {
+    return <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative ">
+
+        <RevealOnScroll>
+
+            <div className="text-center z-10 px-4">
+                <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500
+                         to-purple-600 bg-clip-text text-transparent leading-right">
+                    Welcome to Mike 360
+                </h1>
+
+                <p className="text-grey-400 text-lg mb-8 max-w-lg mx-auto">
+                    Media ya Tanzania Inayo jihusisha na kuhabarisha Ndani na nje ya Taifa
+                    Kwa habari za Siasa, Michezo na Burudani
+                </p>
+
+                <div className="flex justify-center space-x-4">
+                    <a href="#projects"
+                       className="bg-blue-500 text-white py-3 px-6 rounded font-medium transtion relative overflow-hidden
+                             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)">
+                        View News
+                    </a>
+
+                    <a href="#contact"
+                       className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all
+                              duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]
+                              hover:bg-blue-500/10">
+                        Contact us
+                    </a>
+
+                </div>
+
+            </div>
+
+        </RevealOnScroll>
+
+    </section>
+};
